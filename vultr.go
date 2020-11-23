8102ebd1d2a39c4776447d8a167e0b55083e7d09
Alexandre Oliveira <rockytvbr@gmail.com>
2020-11-23 21:05:37 +0100

    Add provider for Vultr

---
+++ vultr.go
@@ -0,0 +1,61 @@
+package vultr
+
+import (
+	vultr "github.com/Xinayder/libdns-vultr"
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+)
+
+// Provider wraps the provider implementation as a Caddy module.
+type Provider struct{ *vultr.Provider }
+
+func init() {
+	caddy.RegisterModule(Provider{})
+}
+
+// CaddyModule returns the Caddy module information.
+func (Provider) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "dns.providers.vultr",
+		New: func() caddy.Module { return &Provider{new(vultr.Provider)} },
+	}
+}
+
+// UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
+//
+// vultr [<api_token>] {
+//     api_token <api_token>
+// }
+//
+func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	repl := caddy.NewReplacer()
+	for d.Next() {
+		if d.NextArg() {
+			p.Provider.APIToken = repl.ReplaceAll(d.Val(), "")
+		}
+		if d.NextArg() {
+			return d.ArgErr()
+		}
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "api_token":
+				if p.Provider.APIToken != "" {
+					return d.Err("API token already set")
+				}
+				p.Provider.APIToken = repl.ReplaceAll(d.Val(), "")
+				if d.NextArg() {
+					return d.ArgErr()
+				}
+			default:
+				return d.Errf("unrecognized subdirective '%s'", d.Val())
+			}
+		}
+	}
+	if p.Provider.APIToken == "" {
+		return d.Err("missing API token")
+	}
+	return nil
+}
+
+// Interface guard
+var _ caddyfile.Unmarshaler = (*Provider)(nil)
